e84e8c81f942a9201e2cb18ae68f77aa10536820
Sebastian <SebastianLionais@cmail.carleton.ca>
2021-09-17 12:51:53 -0400

    Added message to AddressBook main

---
+++ src/AddressBook.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> buddyList;
+
+    public AddressBook (BuddyInfo[] buddyInfos) {
+        this.buddyList = new ArrayList<>(Arrays.asList(buddyInfos));
+    }
+    public AddressBook () {
+        this.buddyList = new ArrayList<>();
+    }
+
+    public void addBuddy (BuddyInfo buddy) {
+        this.buddyList.add(buddy);
+        System.out.println(buddy.getName() + " was added");
+    }
+
+    public void removeBuddy (BuddyInfo buddyInfo) {
+        if (this.buddyList.contains(buddyInfo)) {
+            this.buddyList.remove(buddyInfo);
+            System.out.println(buddyInfo.getName() + " was removed");
+            return;
+        }
+        System.out.println("no buddy of name: " + buddyInfo.getName() + " was found");
+    }
+
+    public void printAddressBook() {
+        for (BuddyInfo buddy: this.buddyList) {
+            System.out.println("-------------------------------------");
+            System.out.println("Name        : " + buddy.getName());
+            System.out.println("Address     : " + buddy.getAddress());
+            System.out.println("Phone Number: " + buddy.getPhoneNumber());
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Address Book");
+    }
+}
